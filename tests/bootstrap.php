a093235e7e777bc1de21aee06a97aeec80599d2b
Christophe Coevoet <stof@notk.org>
2011-08-30 00:03:37 +0200

    Refactored the test for the ListRenderer to make them reusable

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,8 +1,22 @@
 <?php
 
+if (defined('TWIG_LIB_DIR') && 'NOT_SET' !== TWIG_LIB_DIR) {
+    require_once TWIG_LIB_DIR.'/Twig/Autoloader.php';
+    Twig_Autoloader::register();
+}
+
+if (defined('PIMPLE_LIB_DIR') && 'NOT_SET' !== PIMPLE_LIB_DIR) {
+    require_once PIMPLE_LIB_DIR.'/Pimple.php';
+}
+
 spl_autoload_register(function($class) {
-    $file = __DIR__.'/../src/'.str_replace('\\', '/', $class).'.php';
-    if (file_exists($file)) {
-        require $file;
+    $class = ltrim($class, '\\');
+    foreach (array('Knp\Menu\Tests' => __DIR__, 'Knp\Menu' => __DIR__.'/../src') as $namespace => $dir) {
+        if (0 === strpos($class, $namespace)) {
+            $file = $dir.'/'.str_replace('\\', '/', $class).'.php';
+            if (file_exists($file)) {
+                require $file;
+            }
+        }
     }
 });

fd405af917331ed4a2dc25f690856fe8f18a62dc
Jean-François Simon <contact@jfsimon.fr>
2012-10-22 11:48:49 +0200

    Added some tests for 'RouteVoter'.

--- tests/Knp/Menu/Tests/Silex/Voter/RouteVoterTest.php
+++ tests/Knp/Menu/Tests/Silex/Voter/RouteVoterTest.php
@@ -27,26 +27,31 @@ class RouteVoterTest extends \PHPUnit_Framework_TestCase
 
     /**
      * @param string       $route
+     * @param array        $parameters
      * @param string|array $itemRoutes
+     * @param array        $itemsRoutesParameters
      * @param boolean      $expected
      *
      * @dataProvider provideData
      */
-    public function testMatching($route, $itemRoutes, $expected)
+    public function testMatching($route, array $parameters, $itemRoutes, array $itemsRoutesParameters, $expected)
     {
         $item = $this->getMock('Knp\Menu\ItemInterface');
         $item->expects($this->any())
             ->method('getExtra')
             ->with($this->logicalOr($this->equalTo('routes'), $this->equalTo('routesParameters')))
-            ->will($this->returnCallback(function ($parameter) use ($itemRoutes) {
+            ->will($this->returnCallback(function ($parameter) use ($itemRoutes, $itemsRoutesParameters) {
                 switch ($parameter) {
                     case 'routes': return $itemRoutes;
-                    case 'routesParameters': return array();
+                    case 'routesParameters': return $itemsRoutesParameters;
                 }
             }));
 
         $request = new Request();
         $request->attributes->set('_route', $route);
+        foreach ($parameters as $name => $value) {
+            $request->attributes->set($name, $value);
+        }
 
         $voter = new RouteVoter();
         $voter->setRequest($request);
@@ -57,12 +62,33 @@ class RouteVoterTest extends \PHPUnit_Framework_TestCase
     public function provideData()
     {
         return array(
-            'no request route' => array(null, 'foo', null),
-            'no item route' => array('foo', null, null),
-            'same single route' => array('foo', 'foo', true),
-            'different single route' => array('foo', 'bar', null),
-            'matching mutiple routes' => array('foo', array('foo', 'baz'), true),
-            'different multiple routes' => array('foo', array('bar', 'baz'), null),
+            'no request route' => array(null, array(), 'foo', array(), null),
+            'no item route' => array('foo', array(), null, array(), null),
+            'same single route' => array('foo', array(), 'foo', array(), true),
+            'different single route' => array('foo', array(), 'bar', array(), null),
+            'matching mutiple routes' => array('foo', array(), array('foo', 'baz'), array(), true),
+            'different multiple routes' => array('foo', array(), array('bar', 'baz'), array(), null),
+
+            'same single route with different parameters' => array(
+                'foo', array('1' => 'bar'),
+                'foo', array('foo' => array('1' => 'baz')),
+                null
+            ),
+            'same single route with same parameters' => array(
+                'foo', array('1' => 'bar'),
+                'foo', array('foo' => array('1' => 'bar')),
+                true
+            ),
+            'same single route with additional parameters' => array(
+                'foo', array('1' => 'bar'),
+                'foo', array('foo' => array('1' => 'bar', '2' => 'baz')),
+                null
+            ),
+            'same single route with less parameters' => array(
+                'foo', array('1' => 'bar', '2' => 'baz'),
+                'foo', array('foo' => array('1' => 'bar')),
+                true
+            ),
         );
     }
 }

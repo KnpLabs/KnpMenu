cb839bb5036c257e243ccf00174d4a9dfe9171f2
Maxime Veber (aka Nek) <nek.dev@gmail.com>
2014-01-31 15:36:12 +0100

    Removed deprecated methods

--- src/Knp/Menu/MenuFactory.php
+++ src/Knp/Menu/MenuFactory.php
@@ -27,6 +27,11 @@ class MenuFactory implements FactoryInterface
         $this->addExtension(new CoreExtension(), -10);
     }
 
+    /**
+     * @param string $name
+     * @param array $options
+     * @return ItemInterface|MenuItem
+     */
     public function createItem($name, array $options = array())
     {
         foreach ($this->getExtensions() as $extension) {
@@ -52,44 +57,6 @@ class MenuFactory implements FactoryInterface
     {
         $this->extensions[$priority][] = $extension;
         $this->sorted = null;
-    }
-
-    /**
-     * Create a menu item from a NodeInterface
-     *
-     * @deprecated Use \Knp\Menu\Loader\NodeLoader
-     *
-     * @param NodeInterface $node
-     *
-     * @return ItemInterface
-     */
-    public function createFromNode(NodeInterface $node)
-    {
-        trigger_error(__METHOD__ . ' is deprecated. Use Knp\Menu\Loader\NodeLoader instead', E_USER_DEPRECATED);
-
-        $loader = new NodeLoader($this);
-
-        return $loader->load($node);
-    }
-
-    /**
-     * Creates a new menu item (and tree if $data['children'] is set).
-     *
-     * The source is an array of data that should match the output from MenuManipulator->toArray().
-     *
-     * @deprecated Use \Knp\Menu\Loader\ArrayLoader
-     *
-     * @param array $data The array of data to use as a source for the menu tree
-     *
-     * @return ItemInterface
-     */
-    public function createFromArray(array $data)
-    {
-        trigger_error(__METHOD__ . ' is deprecated. Use Knp\Menu\Loader\ArrayLoader instead', E_USER_DEPRECATED);
-
-        $loader = new ArrayLoader($this);
-
-        return $loader->load($data);
     }
 
     /**

67d9de8d27486007c0c58bf264b4fa3095de56dd
Maxime Veber (aka Nek) <nek.dev@gmail.com>
2014-02-28 14:25:50 +0100

    Removed code duplication

--- src/Knp/Menu/Silex/Voter/RouteVoter.php
+++ src/Knp/Menu/Silex/Voter/RouteVoter.php
@@ -5,98 +5,18 @@ namespace Knp\Menu\Silex\Voter;
 use Knp\Menu\ItemInterface;
 use Knp\Menu\Matcher\Voter\VoterInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Knp\Menu\Matcher\Voter\RouteVoter as BaseRouteVoter;
 
 /**
  * Voter based on the route
  */
-class RouteVoter implements VoterInterface
+class RouteVoter extends BaseRouteVoter
 {
-    /**
-     * @var Request
-     */
-    private $request;
-
     public function __construct()
     {
         trigger_error(
             __CLASS__ . ' is deprecated because of namespace, use Knp\\Menu\\Matcher\\RouteVoter instead.',
             E_USER_DEPRECATED
         );
-    }
-
-    public function setRequest(Request $request)
-    {
-        $this->request = $request;
-    }
-
-    public function matchItem(ItemInterface $item)
-    {
-        if (null === $this->request) {
-            return null;
-        }
-
-        $route = $this->request->attributes->get('_route');
-        if (null === $route) {
-            return null;
-        }
-
-        $routes = (array) $item->getExtra('routes', array());
-        $parameters = (array) $item->getExtra('routesParameters', array());
-
-        foreach ($routes as $testedRoute) {
-            if (is_string($testedRoute)) {
-                $testedRoute = array('route' => $testedRoute);
-
-                // BC layer for the configuration of route params
-                if (isset($parameters[$testedRoute['route']])) {
-                    $testedRoute['parameters'] = $parameters[$testedRoute['route']];
-                    trigger_error(
-                        'Using the routeParameters extra is deprecated. The parameters should be passed along the route.',
-                        E_USER_DEPRECATED
-                    );
-                }
-            }
-
-            if (!is_array($testedRoute)) {
-                throw new \InvalidArgumentException('Routes extra items must be strings or arrays.');
-            }
-
-            if ($this->isMatchingRoute($testedRoute)) {
-                return true;
-            }
-        }
-
-        return null;
-    }
-
-    private function isMatchingRoute(array $testedRoute)
-    {
-        $route = $this->request->attributes->get('_route');
-
-        if (isset($testedRoute['route'])) {
-            if ($route !== $testedRoute['route']) {
-                return false;
-            }
-        } elseif (!empty($testedRoute['pattern'])) {
-            if (!preg_match($testedRoute['pattern'], $route)) {
-                return false;
-            }
-        } else {
-            throw new \InvalidArgumentException('Routes extra items must have a "route" or "pattern" key.');
-        }
-
-        if (!isset($testedRoute['parameters'])) {
-            return true;
-        }
-
-        $routeParameters = $this->request->attributes->get('_route_params', array());
-
-        foreach ($testedRoute['parameters'] as $name => $value) {
-            if (!isset($routeParameters[$name]) || (string) $routeParameters[$name] !== (string) $value) {
-                return false;
-            }
-        }
-
-        return true;
     }
 }

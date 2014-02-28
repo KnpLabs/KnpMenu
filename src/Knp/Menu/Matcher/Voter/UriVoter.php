e6d74307945e41e4708fcc8cfcdfd15f536c767f
Maxime Veber (aka Nek) <nek.dev@gmail.com>
2014-02-28 13:52:40 +0100

    Added support for prefix in UriVoter

--- src/Knp/Menu/Matcher/Voter/UriVoter.php
+++ src/Knp/Menu/Matcher/Voter/UriVoter.php
@@ -9,18 +9,46 @@ use Knp\Menu\ItemInterface;
  */
 class UriVoter implements VoterInterface
 {
+    /**
+     * @var string Uri the voter should match on
+     */
     private $uri;
 
-    public function __construct($uri = null)
+    /**
+     * @var bool Setting to know if we should match uri that are prefix to the uri we test
+     */
+    private $matchPrefix;
+
+    /**
+     * @param null $uri
+     * @param bool $matchPrefix
+     */
+    public function __construct($uri = null, $matchPrefix=false)
     {
         $this->uri = $uri;
+        $this->matchPrefix = $matchPrefix;
     }
 
+    /**
+     * @param string $uri
+     */
     public function setUri($uri)
     {
         $this->uri = $uri;
     }
 
+    /**
+     * @param bool $matchPrefix
+     */
+    public function setMatchPrefix($matchPrefix)
+    {
+        $this->matchPrefix = $matchPrefix;
+    }
+
+    /**
+     * @param ItemInterface $item
+     * @return true|null
+     */
     public function matchItem(ItemInterface $item)
     {
         if (null === $this->uri || null === $item->getUri()) {
@@ -28,6 +56,14 @@ class UriVoter implements VoterInterface
         }
 
         if ($item->getUri() === $this->uri) {
+            return true;
+        }
+
+        if ($item->getUri() === '/') {
+            return null;
+        }
+
+        if ($this->matchPrefix && strpos($this->uri, $item->getUri()) === 0) {
             return true;
         }
 
--- tests/Knp/Menu/Tests/Matcher/Voter/UriVoterTest.php
+++ tests/Knp/Menu/Tests/Matcher/Voter/UriVoterTest.php
@@ -15,13 +15,28 @@ class UriVoterTest extends \PHPUnit_Framework_TestCase
      */
     public function testMatching($uri, $itemUri, $expected)
     {
-        $item = $this->getMock('Knp\Menu\ItemInterface');
-        $item->expects($this->any())
-            ->method('getUri')
-            ->will($this->returnValue($itemUri));
+        $item = $this->getItemMock($itemUri);
 
         $voter = new UriVoter();
         $voter->setUri($uri);
+
+        $this->assertSame($expected, $voter->matchItem($item));
+    }
+
+    /**
+     * @param string $uri
+     * @param string $itemUri
+     * @param boolean $expected
+     *
+     * @dataProvider provideDataPrefixSupported
+     */
+    public function testMatchingWithPrefix($uri, $itemUri, $expected)
+    {
+        $item = $this->getItemMock($itemUri);
+
+        $voter = new UriVoter();
+        $voter->setUri($uri);
+        $voter->setMatchPrefix(true);
 
         $this->assertSame($expected, $voter->matchItem($item));
     }
@@ -33,6 +48,26 @@ class UriVoterTest extends \PHPUnit_Framework_TestCase
             'no item uri' => array('foo', null, null),
             'same uri' => array('foo', 'foo', true),
             'different uri' => array('foo', 'bar', null),
+            'prefixed by foo' => array('/foo/bar', '/foo', null),
+            'prefixed by home' => array('/foo', '/', null)
         );
+    }
+
+    public function provideDataPrefixSupported()
+    {
+        $data = $this->provideData();
+        $data['prefixed by foo'][2] = true;
+
+        return $data;
+    }
+
+    private function getItemMock($uri)
+    {
+        $item = $this->getMock('Knp\Menu\ItemInterface');
+        $item->expects($this->any())
+            ->method('getUri')
+            ->will($this->returnValue($uri));
+
+        return $item;
     }
 }

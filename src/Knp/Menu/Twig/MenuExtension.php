1f8191ba48cf3149d1beab24d310250bef535016
Wouter J <waldio.webdesign@gmail.com>
2014-10-31 13:08:36 +0100

    Simplify TwigExtension

--- src/Knp/Menu/Twig/MenuExtension.php
+++ src/Knp/Menu/Twig/MenuExtension.php
@@ -19,37 +19,9 @@ class MenuExtension extends \Twig_Extension
     public function getFunctions()
     {
         return array(
-            'knp_menu_get' => new \Twig_Function_Method($this, 'get'),
-            'knp_menu_render' => new \Twig_Function_Method($this, 'render', array('is_safe' => array('html'))),
+            new \Twig_SimpleFunction('knp_menu_get', array($this->helper, 'get')),
+            new \Twig_SimpleFunction('knp_menu_render', array($this->helper, 'render'), array('is_safe' => array('html'))),
         );
-    }
-
-    /**
-     * Retrieves an item following a path in the tree.
-     *
-     * @param ItemInterface|string $menu
-     * @param array                $path
-     * @param array                $options
-     *
-     * @return ItemInterface
-     */
-    public function get($menu, array $path = array(), array $options = array())
-    {
-        return $this->helper->get($menu, $path, $options);
-    }
-
-    /**
-     * Renders a menu with the specified renderer.
-     *
-     * @param ItemInterface|string|array $menu
-     * @param array                      $options
-     * @param string                     $renderer
-     *
-     * @return string
-     */
-    public function render($menu, array $options = array(), $renderer = null)
-    {
-        return $this->helper->render($menu, $options, $renderer);
     }
 
     /**

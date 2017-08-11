a3b30db3d43652a51f3b2f604a630f311a046344
Christophe Coevoet <stof@notk.org>
2017-08-11 20:12:31 +0200

    Implement a menu provider building menu thanks to lazy-loaded builder
    The usage of a Closure to lazily instantiate the object of the callable is
    inspired by Symfony EventDispatcher 3.3+, allowing to use the lazy-loading
    features of Symfony DI 3.3+ without injecting the container.

---
+++ src/Knp/Menu/Provider/LazyProvider.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Knp\Menu\Provider;
+
+/**
+ * A menu provider building menus lazily thanks to builder callables.
+ *
+ * Builders can either be callables or a factory for an object callable
+ * represented as array(Closure, method), where the Closure gets called
+ * to instantiate the object.
+ */
+class LazyProvider implements MenuProviderInterface
+{
+    private $builders;
+
+    public function __construct(array $builders)
+    {
+        $this->builders = $builders;
+    }
+
+    public function get($name, array $options = array())
+    {
+        if (!isset($this->builders[$name])) {
+            throw new \InvalidArgumentException(sprintf('The menu "%s" is not defined.', $name));
+        }
+
+        $builder = $this->builders[$name];
+
+        if (is_array($builder) && isset($builder[0]) && $builder[0] instanceof \Closure) {
+            $builder[0] = $builder[0]();
+        }
+
+        if (!is_callable($builder)) {
+            throw new \LogicException(sprintf('Invalid menu builder for "%s". A callable or a factory for an object callable are expected.', $name));
+        }
+
+        return call_user_func($builder, $options);
+    }
+
+    public function has($name, array $options = array())
+    {
+        return isset($this->builders[$name]);
+    }
+}
